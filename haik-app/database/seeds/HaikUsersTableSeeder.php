c28900bb3f5b78b6fd64d0cec00fa8005a382f0c
Shun Hotta <stars.big2men@gmail.com>
2014-05-09 16:38:04 +0900

    Add User seeder and User Model

---
+++ haik-app/database/seeds/HaikUsersTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+class HaikUsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('haik_users')->truncate();
+
+        User::create(array(
+                'name' => 'admin',
+                'email' => 'user@example.com',
+                'password' => 'hogehoge',
+        ));
+        
+    }
+}
