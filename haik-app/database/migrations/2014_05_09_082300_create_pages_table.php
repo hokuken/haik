39a30d27656a237cd7ac762c7163ee854904c4ef
Shun Hotta <stars.big2men@gmail.com>
2014-05-12 17:02:46 +0900

    Modify migration of pages table

--- haik-app/database/migrations/2014_05_09_082300_create_pages_table.php
+++ haik-app/database/migrations/2014_05_09_082300_create_pages_table.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePagesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-      Schema::create('haik_pages', function($table)
-      {
-           $table->increments('id')->unsigned();
-           $table->string('name')->unique();
-           $table->text('body');
-           $table->integer('body_version')->unsigined()->default(0);
-           $table->softDeletes();
-           $table->timestamps();
-      });
+        Schema::create('haik_pages', function($table)
+        {
+            $table->increments('id')->unsigned();
+            $table->string('name')->unique();
+            $table->text('body');
+            $table->integer('body_version')->unsigined()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
     }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('haik_pages');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('haik_pages');
+    }
 
 }
