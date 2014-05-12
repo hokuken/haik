8a23ab711e7c898c6e38d0cd0503c388b9602859
Shun Hotta <stars.big2men@gmail.com>
2014-05-12 12:32:39 +0900

    Modify EditPlugin (easy mode)

--- haik-app/plugins/edit/EditPlugin.php
+++ haik-app/plugins/edit/EditPlugin.php
@@ -1,10 +1,29 @@
 <?php
 namespace Hokuken\Haik\Plugin\Edit;
 
-use RuntimeException;
+use App;
+use Config;
 use Hokuken\Haik\Plugin\BasePlugin;
+use RuntimeException;
 
 
 class EditPlugin extends BasePlugin {
+
+    public function inline($params = array(), $body = '')
+    {
+        if (count($params) > 0)
+        {
+            $pagename = array_pop($params);
+        }
+        else if (App::isAlias('page.current'))
+        {
+            $pagename = App::make('page.current');
+        }
+        else
+        {
+            $pagename = Config::get('haik.page.default');
+        }
+        return link_to_route('plugin.edit', $body, $pagename);
+    }
 
 }
