2fbe400a4df19b88b6deb0fe3f861b1594e59865
Shun Hotta <stars.big2men@gmail.com>
2014-05-13 19:23:43 +0900

    Modify Page\PageMeta using FragmentBag

--- haik-app/tests/Hokuken/Haik/Page/PageMetaTest.php
+++ haik-app/tests/Hokuken/Haik/Page/PageMetaTest.php
@@ -41,159 +41,56 @@ class PageMetaTest extends TestCase {
 
     public function setUpData()
     {
-        $this->pageMeta->setAll($this->pageMeta->read(), true);
+        $this->pageMeta->setAll($this->pageMeta->read()->getAll(), true);
     }
 
     public function testGetPage()
     {
         $this->assertEquals($this->page, $this->pageMeta->getPage());
     }
+
     public function testRead()
     {
-        $data = $this->pageMeta->read();
+        $data = $this->pageMeta->read()->getAll();
         $this->assertEquals($this->testData, $data);
     }
 
-    public function testGet()
+    public function testSave()
     {
-        $this->setUpData();
+        $key = 'foo.bar';
+        $value = 'buzz';
+        $this->pageMeta->set($key, $value);
+        $this->pageMeta->save();
+        $page = $this->pageMeta->getPage();
 
-        $value = $this->pageMeta->get('title');
-        $this->assertEquals('TestTitle', $value);
-
-        $value = $this->pageMeta->get('unknown');
-        $this->assertNull($value);
-
-        $value = $this->pageMeta->get('unknown', 'foobar');
-        $this->assertEquals('foobar', $value);
-
-        $value = $this->pageMeta->get('group.key');
-        $this->assertEquals('value', $value);
-
-        $value = $this->pageMeta->get('group.unknown');
-        $this->assertNull($value);
-        
-        $value = $this->pageMeta->get('unknowngroup.unknown');
-        $this->assertNull($value);
+        $fragment = PageMetaFragment::where('haik_page_id', $page->id)->where('key', $key)->first();
+        $this->assertEquals($value, $fragment->value);
     }
 
-    public function testGetAll()
+    public function testSaveWithoutPage()
     {
-        $this->setUpData();
+        $page = new Page();
+        $page_meta = new PageMeta($page);
+        $key = 'hoge';
+        $value = 'value';
 
-        $data = $this->pageMeta->getAll();
-        $this->assertEquals($this->testData, $data);
+        PageMetaFragment::where('key', $key)->delete();
+
+        $page_meta->set($key, $value);
+        $page_meta->save();
+
+        $fragment = PageMetaFragment::where('key', $key)->first();
+        $this->assertNull($fragment);
+
     }
 
     public function testToYaml()
     {
         $this->setUpData();
+        $expected = $this->pageMeta->getAll();
         $yaml = $this->pageMeta->toYaml();
-        $expected = Yaml::dump($this->testData);
-        $this->assertEquals($expected, $yaml);
-    }
-
-    public function testHas()
-    {
-        $this->setUpData();
-        $this->assertTrue($this->pageMeta->has('title'));
-        $this->assertFalse($this->pageMeta->has('unknown'));
-    }
-
-    public function testSet()
-    {
-        $expected = 'test title';
-        $this->pageMeta->set('title', $expected);
-        $value = $this->pageMeta->get('title');
-        $this->assertEquals($expected, $value);
-
-        $expected = 'grouped value';
-        $this->pageMeta->set('group.value', $expected);
-        $value = $this->pageMeta->get('group.value');
-        $this->assertEquals($expected, $value);
-
-        $expected = 'new value';
-        $this->pageMeta->set('newkey', $expected);
-        $value = $this->pageMeta->get('newkey');
-        $this->assertEquals($expected, $value);
-
-        $expected = 'more.nested.value';
-        $this->pageMeta->set('more.nested.value', $expected);
-        $value = $this->pageMeta->get('more.nested.value');
-        $this->assertEquals($expected, $value);
-
-    }
-
-    public function testSetAll()
-    {
-        $this->pageMeta->setAll($this->testData);
-        $data = $this->pageMeta->getAll();
-        $this->assertEquals($this->testData, $data);
-//        $this->assertAttributeEquals($this->testData, 'data', $this->pageMeta);
-    }
-
-    public function testIsDirtyWhenSetNewKey()
-    {
-        $this->setUpData();
-        $this->pageMeta->set('newkey', 'new value');
-        $this->assertTrue($this->pageMeta->isDirty());
-    }
-
-    public function testIsDirtyWhenSetExistedKey()
-    {
-        $this->setUpData();
-        $this->pageMeta->set('title', 'new title');
-        $this->assertTrue($this->pageMeta->isDirty());
-    }
-
-    public function testIsDirtyWhenSetSameValueToExistedKey()
-    {
-        $this->setUpData();
-        $this->pageMeta->set('title', 'TestTitle');
-        $this->assertFalse($this->pageMeta->isDirty());
-    }
-
-    public function testIsDirtyWhenRemoveExistedKey()
-    {
-        $this->setUpData();
-        $this->pageMeta->remove('title');
-        $this->assertTrue($this->pageMeta->isDirty());
-    }
-
-    public function testIsDirtyWhenReadData()
-    {
-        $this->setUpData();
-        $this->pageMeta->set('title', 'new title');
-        $this->assertTrue($this->pageMeta->isDirty());
-
-        $this->pageMeta->read();
-        $this->assertFalse($this->pageMeta->isDirty());
-    }
-    
-    public function testSave()
-    {
-        $this->setUpData();
-        $this->pageMeta->set('newkey', 'new value');
-        $this->pageMeta->set('title', 'UpdatedTitle');
-        $this->pageMeta->save();
-
-        $expected = $this->testData;
-        $expected['newkey'] = 'new value';
-        $expected['title'] = 'UpdatedTitle';
-
-        $data = $this->pageMeta->read();
-        
+        $data = Yaml::parse($yaml);
         $this->assertEquals($expected, $data);
-    }
-
-    public function testDelete()
-    {
-        $this->pageMeta->delete();
-        $data = $this->pageMeta->getAll();
-        $this->assertEquals(array(), $data);
-
-        $data = $this->pageMeta->read();
-        $this->assertEquals(array(), $data);
     }
 
 }
