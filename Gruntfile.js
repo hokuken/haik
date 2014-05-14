0663f68213c673c2184ffa994d76cfd888aed888
masamori <r.masamori@gmail.com>
2014-05-14 14:15:29 +0900

    Add grunt task with less compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,52 @@
 module.exports = function(grunt) {
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
+  var path = require('path');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        exec: {
-            server: {
-                cmd: "php artisan serv"
+  // for compile less files
+  var lessConfig = {
+      development: {
+        options: {
+          compress: false
+        },
+        files: [
+          {
+            expand: true,
+            cwd: './haik-contents/themes/',
+            src: [
+              './*/less/theme.less'
+            ],
+            dest: './',
+            rename: function(dest, src){
+              return path.normalize(this.cwd + src.replace(/less/g, 'css'));
             }
-        }
-    });
+          }
+        ]
+      }
+  };
 
-    grunt.registerTask('server', ['exec:server'])
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: lessConfig,
+  	watch: {
+      less: {
+        files: [
+          './haik-contents/themes/*/less/*.less'
+        ],
+        tasks: ['less'],
+        options: {
+          liveoverload: true
+        }
+      },
+    },
+    exec: {
+      server: {
+        cmd: "php artisan serv"
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+  grunt.registerTask('publish', ['less']);
+  grunt.registerTask('server', ['exec:server']);
 }
