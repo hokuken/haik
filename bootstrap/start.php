307d1ad17bd919d29173562f647819f14c55c82a
masamori <r.masamori@gmail.com>
2014-05-14 11:20:57 +0900

    Add dev debug mode

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('McDaijiro.local'),
+	'local' => array('McDaijiro.local', 'mac-rmasamori.local'),
 
 ));
 
