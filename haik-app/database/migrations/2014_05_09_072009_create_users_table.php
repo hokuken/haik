f54d2b5bba2b04aa53d7efaf4f69e0198a5700b1
masamori <r.masamori@gmail.com>
2014-05-15 11:20:49 +0900

    Add remember_token field for auth

--- haik-app/database/migrations/2014_05_09_072009_create_users_table.php
+++ haik-app/database/migrations/2014_05_09_072009_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration {
             $table->string('email')->unique();
             $table->string('name');
             $table->string('password');
+            $table->string('remember_token')->after('picture')->default('');
             $table->timestamps();
         });
 	}
