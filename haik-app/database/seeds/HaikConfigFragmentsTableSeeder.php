51831d8af4e786bb165392b62d90c35558e93113
masamori <r.masamori@gmail.com>
2014-05-15 13:23:53 +0900

    Add theme config and read selected theme

--- haik-app/database/seeds/HaikConfigFragmentsTableSeeder.php
+++ haik-app/database/seeds/HaikConfigFragmentsTableSeeder.php
@@ -7,13 +7,18 @@ class HaikConfigFragmentsTableSeeder extends Seeder {
         DB::table('haik_config_fragments')->truncate();
 
         ConfigFragment::create(array(
-            'key' => 'site.title',
+            'key' => 'title',
             'value' => 'ha::::k'
         ));
 
         ConfigFragment::create(array(
-            'key' => 'site.author',
+            'key' => 'author',
             'value' => 'issa'
+        ));
+
+        ConfigFragment::create(array(
+            'key' => 'theme.name',
+            'value' => 'ikk'
         ));
     }
 }
