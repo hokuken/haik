a95fc879e1631a925efd4d445b959f0482c88d4d
Shun Hotta <stars.big2men@gmail.com>
2014-05-19 14:48:18 +0900

    Modify filr plugin controller
    Show form.
    Upload and save file.

--- haik-app/plugins/Filr/FilrPluginController.php
+++ haik-app/plugins/Filr/FilrPluginController.php
@@ -2,6 +2,13 @@
 namespace Hokuken\Haik\Plugin\Filr;
 
 use BaseController;
+use Config;
+use File;
+use Input;
+use Redirect;
+use View;
+use Filr;
+use Symfony\Component\HttpFoundation\File\Exception\UploadException;
 
 class FilrPluginController extends BaseController {
 
@@ -20,7 +27,7 @@ class FilrPluginController extends BaseController {
      */
     public function showForm()
     {
-
+        return View::make('file.upload');
     }
 
     /**
@@ -30,7 +37,30 @@ class FilrPluginController extends BaseController {
      */
     public function upload()
     {
+        $name = 'file';
+        if ( ! Input::hasFile($name))
+            return Redirect::route('plugin.filr.upload');
+        if ( ! Input::file($name)->isValid())
+            throw new UploadException("Uploaded file is not valid");
 
+        $file = new Filr;
+        $uploaded_file = Input::file($name);
+
+        $file->title = $uploaded_file->getClientOriginalName();
+        $file->size = $uploaded_file->getSize();
+        $file->setType($uploaded_file);
+        $file->setFilePath();
+
+        $file->save();
+
+        // move to storage
+        $path = Config::get('haik.file.path').'/'.$file->filepath;
+        $dirname = dirname($path);
+        $basename = basename($path);
+		File::makeDirectory($dirname, 0777, true, true);
+		$uploaded_file->move($dirname, $basename);
+
+        return Redirect::route('plugin.filr.upload');
     }
 
 }
