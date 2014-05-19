e3e196fc4cd8e98fccaf11dd93b4bbb9b4e79a51
Shun Hotta <stars.big2men@gmail.com>
2014-05-19 11:03:55 +0900

    Add stab controller

---
+++ haik-app/plugins/Filr/FilrPluginController.php
@@ -0,0 +1,36 @@
+<?php
+namespace Hokuken\Haik\Plugin\Filr;
+
+use BaseController;
+
+class FilrPluginController extends BaseController {
+
+    /**
+     * Show file list
+     *
+     * @return View
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show upload form
+     */
+    public function showForm()
+    {
+
+    }
+
+    /**
+     * Upload file
+     *
+     * @return Redirect
+     */
+    public function upload()
+    {
+
+    }
+
+}
