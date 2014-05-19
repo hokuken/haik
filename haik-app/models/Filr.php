efc9b6490198f0f396397d14dcd5d8401f7c071a
Shun Hotta <stars.big2men@gmail.com>
2014-05-19 15:30:32 +0900

    Modify Filr model
    Add formatted size accessor

--- haik-app/models/Filr.php
+++ haik-app/models/Filr.php
@@ -161,4 +161,40 @@ class Filr extends Eloquent {
         return $path;
     }
 
+    /**
+     * Get GB/MB/KB/bytes Formatted size string
+     *
+     * @return string formatted size
+     */
+    public function getFormattedSizeAttribute()
+    {
+        $bytes = $this->size;
+
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+    }
 }
